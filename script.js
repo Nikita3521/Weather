c2056094ada86b3531e03755265fd5f84a9290f6
secretive20 <2fortnite2lol@gmail.com>
2025-11-09 15:36:07 +0200

    almost all

---
+++ script.js
@@ -0,0 +1,31 @@
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=Chernihiv&appid=d65ca3417d3624d11288475acc964b98&units=metric';
+let degree = document.querySelector('.degree')
+let speedWind = document.querySelector('.speed-wind')
+let humidityPercent = document.querySelector('.humidity-percent')
+let weatherImg = document.getElementById('img')
+const options = {
+    method: 'GET'
+};
+async function getWeather() {
+    try {
+        const response = await fetch(url, options);
+        const data = await response.json();
+        console.log(data);
+        degree.textContent = parseInt(data.main.temp)
+        speedWind.textContent = data.wind.speed
+        humidityPercent.textContent = data.main.humidity
+        if (data.weather[0].main === 'Clouds') {
+            weatherImg.src = '/img/cloud.png';
+        } else if (data.weather[0].main === 'Rain') {
+            weatherImg.src = '/img/rain.png';
+        } else if (data.weather[0].main === 'sun') {
+            weatherImg.src = '/img/sun.png';
+        }
+    } catch (error) {
+        console.error(error);
+    }
+
+}
+
+getWeather()
+
